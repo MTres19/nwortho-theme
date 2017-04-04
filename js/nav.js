299d91c3374b751c831d95ee2c87ab48db55c5f1
Matthew Trescott <matthewtrescott@gmail.com>
2017-04-03 20:01:46 -0400

    Mobile responsive swipable navbar

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,13 @@
 // Thanks https://www.templatemonster.com/blog/build-navigation-menu-css-jquery/
 
-$(document).ready(function() {
-    var $toggleButton = $('#header-bar-menu-button');
+function mobileMenuToggle()
+{
     var $fullPageShadow = $('#full-page-shadow');
-    
-    $toggleButton.on('click', function() {
+    var $leftNavDrawer = $('#left-nav-drawer');
+    if ($('#header-bar-title').css('display') == 'none') // Mobile only (detect breakpoint)
+    {
+        $leftNavDrawer.toggleClass('left-nav-drawer-hidden');
+        
         if ($fullPageShadow.css('opacity') != 0)
         {
             $fullPageShadow.css('opacity', 0);
@@ -14,6 +17,33 @@ $(document).ready(function() {
         if ($fullPageShadow.css('opacity') == 0)
         {
             $fullPageShadow.css('opacity', 0.3);
+        }
+    }
+}
+
+$(document).ready(function() {
+    var $toggleButton = $('#header-bar-menu-button');
+    var $navDrawerSwipeDetector = $('#nav-drawer-swipe-detector');
+    
+    // Menu button
+    $toggleButton.on('click', mobileMenuToggle);
+    
+    // Menu swipe
+    $navDrawerSwipeDetector.swipe({
+        swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+            if ($('#left-nav-drawer').hasClass('left-nav-drawer-hidden'))
+            {
+                mobileMenuToggle();
+            }
+        }
+    });
+    
+    $('#left-nav-drawer').swipe({
+        swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+            if (!$('#left-nav-drawer').hasClass('left-nav-drawer-hidden'))
+            {
+                mobileMenuToggle();
+            }
         }
     });
 });
