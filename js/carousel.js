63629743744f7b5eeb990772433e07b008107b3f
Matthew Trescott <matthewtrescott@gmail.com>
2017-04-05 17:21:42 -0400

    Add carousel advance button

--- js/carousel.js
+++ js/carousel.js
@@ -1,51 +1,56 @@
-$(document).ready(function() {
-    window.setInterval(function() {
-        var $carousel_panels = $('.carousel-panel');
-        
-        for (var i = 0; i < $carousel_panels.length; i++)
+function advanceCarousel()
+{
+    var $carousel_panels = $('.carousel-panel');
+    
+    for (var i = 0; i < $carousel_panels.length; i++)
+    {
+        // Find current panel
+        if ($carousel_panels.eq(i).hasClass('carousel-current-panel'))
         {
-            // Find current panel
-            if ($carousel_panels.eq(i).hasClass('carousel-current-panel'))
-            {
-                var $current_carousel = $carousel_panels.eq(i);
-            }
-            
-            // Find previous panel (if there is one)
-            if ($carousel_panels.eq(i).hasClass('carousel-previous-panel'))
-            {
-                var $previous_carousel = $carousel_panels.eq(i);
-            }
+            var $current_carousel = $carousel_panels.eq(i);
         }
         
-        if (typeof $current_carousel === 'undefined')
+        // Find previous panel (if there is one)
+        if ($carousel_panels.eq(i).hasClass('carousel-previous-panel'))
         {
-            var $current_carousel = $carousel_panels.eq(0);
-            $current_carousel.toggleClass('carousel-current-panel');
-            return; // First time
+            var $previous_carousel = $carousel_panels.eq(i);
         }
-        
-        console.log("Current carousel (before modification):");
-        console.log($current_carousel);
-        
+    }
+    
+    if (typeof $current_carousel === 'undefined')
+    {
+        var $current_carousel = $carousel_panels.eq(0);
         $current_carousel.toggleClass('carousel-current-panel');
-        $current_carousel.toggleClass('carousel-previous-panel'); // Move current panel away
-        
-        if ($carousel_panels.index($current_carousel) === $carousel_panels.length - 1)
-        {
-            $current_carousel = $carousel_panels.eq(0);
-        }
-        else
-        {
-            $current_carousel = $carousel_panels.eq($carousel_panels.index($current_carousel) + 1);
-        }
-        
-        $current_carousel.toggleClass('carousel-current-panel'); // Move in new current panel
-        
-        // Move previous carousel back, behind the current carousel
-        if (typeof $previous_carousel !== 'undefined')
-        {
-            $previous_carousel.toggleClass('carousel-previous-panel');
-        }
-        
-    }, 5000);
+        return; // First time
+    }
+    
+    $current_carousel.toggleClass('carousel-current-panel');
+    $current_carousel.toggleClass('carousel-previous-panel'); // Move current panel away
+    
+    if ($carousel_panels.index($current_carousel) === $carousel_panels.length - 1)
+    {
+        $current_carousel = $carousel_panels.eq(0);
+    }
+    else
+    {
+        $current_carousel = $carousel_panels.eq($carousel_panels.index($current_carousel) + 1);
+    }
+    
+    $current_carousel.toggleClass('carousel-current-panel'); // Move in new current panel
+    
+    // Move previous carousel back, behind the current carousel
+    if (typeof $previous_carousel !== 'undefined')
+    {
+        $previous_carousel.toggleClass('carousel-previous-panel');
+    }
+}
+
+$(document).ready(function() {
+    var carousel_id = window.setInterval(advanceCarousel, 5000);
+    
+    $('#carousel-advance-button').on('click', function() {
+        advanceCarousel();
+        window.clearInterval(carousel_id); // Give the user some time to view the panel
+        carousel_id = window.setInterval(advanceCarousel, 5000); // Start the carousel again
+    });
 });
