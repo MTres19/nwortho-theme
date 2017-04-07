07ba69c7d080ed16eafb10e283dc867cf9fb8936
Matthew Trescott <matthewtrescott@gmail.com>
2017-04-07 15:05:41 -0400

    Improvements to footer---layout and appearance

--- js/footer.js
+++ js/footer.js
@@ -4,8 +4,34 @@ function adjustFooter()
     $('div.content-wrapper').css('margin-bottom', footerHeight);
 }
 
+function adjustFooterFlexDirection(footerLinksHorizWidth)
+{
+    var $footerLinks = $('section#footer-links');
+    var contentFooterWidth = $('footer#content-footer').width();
+    var footerLogoWidth = $('div#footer-logo-container').outerWidth();
+    
+    if (footerLinksHorizWidth >= contentFooterWidth - footerLogoWidth
+        && $footerLinks.css('flex-direction') === 'row'
+    )
+    {
+        $footerLinks.css('flex-direction', 'column');
+    }
+    else if (footerLinksHorizWidth < contentFooterWidth - footerLogoWidth
+        && $footerLinks.css('flex-direction') === 'column'
+    )
+    {
+        $footerLinks.css('flex-direction', 'row');
+    }
+}
+
 $(document).ready(function() {
+    var footerLinksHorizWidth = $('section#footer-links').width();
+    adjustFooterFlexDirection(footerLinksHorizWidth);
     adjustFooter();
     
+    $(window).resize(function() {
+        adjustFooterFlexDirection(footerLinksHorizWidth);
+    });
     $(window).resize(adjustFooter);
+
 });
