4a1bb5f15bcd113fc79e209f9decd2a14a972a64
Matthew Trescott <matthewtrescott@gmail.com>
2017-04-09 20:21:15 -0400

    Improve footer appearance on very small devices
    E.g. iPhone 5, iPhone SE

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,21 @@
 function adjustFooter()
 {
-    var footerHeight = $('#content-footer').outerHeight();
-    $('div.content-wrapper').css('margin-bottom', footerHeight);
+    var footerHeight = $('footer#content-footer').outerHeight();
+    
+    if (footerHeight <= $(window).height() - $('header.fixed-header-bar').outerHeight())
+    {
+        $('div.content-wrapper').css('margin-bottom', footerHeight);
+        $('footer#content-footer').css('position', 'fixed');
+    }
+    else
+    {
+        /* In this case, the screen is not tall enough to display the
+         * entire height of the footer. The margin on the content-wrapper
+         * is removed and the footer loses its special curtain effect.
+         */
+        $('div.content-wrapper').css('margin-bottom', '');
+        $('footer#content-footer').css('position', 'relative');
+    }
 }
 
 function adjustFooterFlexDirection(footerLinksHorizWidth)
@@ -12,7 +26,7 @@ function adjustFooterFlexDirection(footerLinksHorizWidth)
     
     if (footerLinksHorizWidth >= contentFooterWidth - footerLogoWidth
         && $footerLinks.css('flex-direction') === 'row'
-    )
+    ) // Must have "or equal to" b/c otherwise flex-wrap: wrap might engage on #content-footer
     {
         $footerLinks.css('flex-direction', 'column');
     }
@@ -24,14 +38,36 @@ function adjustFooterFlexDirection(footerLinksHorizWidth)
     }
 }
 
+function adjustFooterLogoAlignment()
+{
+    var contentFooterWidth = $('footer#content-footer').width();
+    
+    var $footerLogo = $('div#footer-logo-container');
+    var footerLogoWidth = $footerLogo.outerWidth();
+    var footerLinksWidth = $('section#footer-links').outerWidth();
+    
+    if (footerLinksWidth + footerLogoWidth > contentFooterWidth)
+    {
+        $footerLogo.css('margin-left', 'auto'); // Align center
+        $footerLogo.css('margin-right', 'auto');
+    }
+    else
+    {
+        $footerLogo.css('margin-left', ''); // Align right
+        $footerLogo.css('margin-right', '');
+    }
+}
+
 $(document).ready(function() {
     var footerLinksHorizWidth = $('section#footer-links').width();
     adjustFooterFlexDirection(footerLinksHorizWidth);
     adjustFooter();
+    adjustFooterLogoAlignment();
     
     $(window).resize(function() {
         adjustFooterFlexDirection(footerLinksHorizWidth);
+        adjustFooter();
+        adjustFooterLogoAlignment();
     });
-    $(window).resize(adjustFooter);
 
 });
