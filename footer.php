ffab238acf769ce8f905feb21de1fc6e8abae606
Matthew Trescott <matthewtrescott@gmail.com>
2017-04-11 20:24:52 -0400

    Beginnings of WordPress integration

---
+++ footer.php
@@ -0,0 +1,41 @@
+  <!-- Footer
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <footer id="content-footer">
+    <section id="footer-links">
+      <section class="footer-link-list">
+        <h1>For Patients</h1>
+        <ul>
+          <li><a>Products</a></li>
+          <li><a>Services</a></li>
+          <li><a>Location</a></li>
+        </ul>
+      </section>
+      <section class="footer-link-list">
+        <h1>For Physicians</h1>
+        <ul>
+          <li><a>Partnership</a></li>
+          <li><a>Business cards</a></li>
+        </ul>
+      </section>
+      <section class="footer-link-list">
+        <h1>About</h1>
+        <ul>
+          <li><a>The business</a></li>
+          <li><a>Legal</a></li>
+          <li><a>This website</a></li>
+        </ul>
+      </section>
+    </section>
+    <div id="footer-logo-container">
+      <span id="footer-logo-icon" class="icm-nworthotics-logo"></span>
+      <span id="footer-logo-nw">NORTHWEST</span>
+      <span id="footer-logo-nw-subtitle">Orthotics—Prosthetics</span>
+    </div>
+  </footer>
+  
+  <?php wp_footer();?>
+  
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+</html> 
---
+++ nav.php
@@ -0,0 +1,16 @@
+  <!-- Navigation
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <nav id="left-nav-drawer" class="left-nav-drawer-hidden">
+    <ul id="left-nav-links-list">
+      <li id="left-nav-link-home">Home</li>
+      <li id="left-nav-link-about">About</li>
+      <li id="left-nav-link-services">Services</li>
+      <li id="left-nav-link-team">Team</li>
+    </ul>
+  </nav>
+  
+  <div id="nav-drawer-swipe-detector"></div>
+  
+  <!-- Shadow for navigation menu
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <div id="full-page-shadow"></div> 
