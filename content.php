d6606da6476de78ac4ba04686b55ea21cbdc8963
Matthew Trescott <matthewtrescott@gmail.com>
2017-04-14 15:59:34 -0400

    Remove unused files

--- content.php
+++
@@ -1,5 +0,0 @@
-<div class="text-wrapper">
-    <h1 class="blog-post-title"><?php the_title(); ?></h1>
-    <div class="blog-post-info">Written by <?php the_author(); ?> on <?php the_date(); ?></div>
-    <?php the_content(); ?>
-</div>
