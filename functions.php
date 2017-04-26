354e20bf92186dac2edf183cd8a4ec85cbfd67f8
Matthew Trescott <matthewtrescott@gmail.com>
2017-04-26 11:57:24 -0400

    Clean up functions.php a bit

--- functions.php
+++ functions.php
@@ -1,17 +1,17 @@
 <?php
 
-function register_nwortho_menus()
-{
-    register_nav_menus(
-        array(
-            'footer-menu-left' => 'Left/top footer menu',
-            'footer-menu-middle' => 'Middle footer menu',
-            'footer-menu-right' => 'Right/bottom footer menu',
-            'left-nav-menu' => 'Left Navigation Drawer'
-        )
-    );
-}
-add_action('init', 'register_nwortho_menus');
+add_action('init', function()
+    {
+        register_nav_menus(
+            array(
+                'footer-menu-left' => 'Left/top footer menu',
+                'footer-menu-middle' => 'Middle footer menu',
+                'footer-menu-right' => 'Right/bottom footer menu',
+                'left-nav-menu' => 'Left Navigation Drawer'
+            )
+        );
+    }
+);
 
 
 function fallback_left_menu()
@@ -29,32 +29,32 @@ add_theme_support('html5');
 
 
 // From https://peterwilson.cc/including-wordpress-comment-reply-js/
-function theme_queue_js()
-{
-    if (is_singular() && comments_open() && get_option('thread_comments'))
+add_action('wp_print_scripts', function()
     {
-        wp_enqueue_script( 'comment-reply' );
+        if (is_singular() && comments_open() && get_option('thread_comments'))
+        {
+            wp_enqueue_script( 'comment-reply' );
+        }
     }
-}
-add_action('wp_print_scripts', 'theme_queue_js');
+);
 
 // Make sure users can edit the "Posts page" since this theme displays that page's content at the top
 // Thanks https://robincornett.com/posts-page/
-function allow_posts_page_to_be_edited($post)
-{
-    if ($post->ID = get_option('page_for_posts'))
+add_action('edit_form_after_title', function($post)
     {
-        add_post_type_support('page', 'editor');
+        if ($post->ID == get_option('page_for_posts'))
+        {
+            add_post_type_support('page', 'editor');
+        }
     }
-}
-add_action('edit_form_after_title', 'allow_posts_page_to_be_edited');
+);
 
 // Thanks to https://wordpress.stackexchange.com/a/45708
 function _nw_get_menu_by_location($location) {
-    if( empty($location) ) return false;
+    if ( empty($location) ) return false;
 
     $locations = get_nav_menu_locations();
-    if( ! isset( $locations[$location] ) ) return false;
+    if (!isset($locations[$location])) return false;
 
     $menu_obj = get_term( $locations[$location], 'nav_menu' );
 
