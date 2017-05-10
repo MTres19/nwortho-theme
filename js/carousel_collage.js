a67554536d46b5a7f44401214c8927407d912f51
Matthew Trescott <matthewtrescott@gmail.com>
2017-05-09 21:07:22 -0400

    Make carousel layout script work properly, fine tuning still TODO

--- js/carousel_collage.js
+++ js/carousel_collage.js
@@ -1,33 +1,39 @@
 function redistributeCollagePics()
 {
-    var $pictures = jQuery('.carousel-panel-collage-wrapper').contents();
-    jQuery('.carousel-panel-collage-wrapper').parent().remove();
+    var $pictures = jQuery('.carousel-panel-collage-wrapper').children();
     var $container = jQuery('#carousel-container');
     var width = $container.innerWidth();
     var height = $container.innerHeight();
     
     // Calculate number of pictures per panel
-    var picsPerPanel = Math.floor(($pictures.length * 400 * 220) / (width * height));
-    var numPanels = Math.ceil($pictures.length / picsPerPanel);
+    var numPanels = Math.ceil(($pictures.length * 400 * 220) / (width * height));
+    var picsPerPanel = Math.ceil($pictures.length / numPanels);
+    
     
     var $panels = jQuery();
     for (var i = 0; i < numPanels; ++i)
     {
+        var $wrapper = jQuery('<div class="carousel-panel-collage-wrapper"></div>');
+        
         for (var j = i * picsPerPanel; j < (i * picsPerPanel) + picsPerPanel; ++j)
         {
-            $wrapper = jQuery('<div class="carousel-panel-collage-wrapper"></div>');
-            $wrapper.html($pictures.eq(j));
-            
-            $panel = jQuery('<div class="carousel-panel"></div>');
-            $panel.append($wrapper);
-            
-            $panels.append($panel);
+            $wrapper.append($pictures.eq(j));
         }
+        
+        var $panel = jQuery('<div class="carousel-panel"></div>');
+        $panel.append($wrapper);
+        
+        $panels = $panels.add($panel);
     }
+    var $old_wrappers = jQuery('.carousel-panel-collage-wrapper');
+    $old_wrappers.eq(0).parent().after($panels);
+    $old_wrappers.parent().remove();
+    
 }
 
 jQuery(document).ready(function()
     {
+        redistributeCollagePics();
         jQuery(window).resize(redistributeCollagePics);
     }
 );
